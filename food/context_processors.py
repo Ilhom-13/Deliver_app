3466394280e160ab0db81328948b426a3bc3bbd2
Ilhom-13 <84261628+Ilhom-13@users.noreply.github.com>
2021-11-24 17:27:16 +0500

    Delete food directory

--- food/context_processors.py
+++
@@ -1,9 +0,0 @@
-from food.models import Restaurant, Category
-
-def all_restaurants_categories(request):
-
-    """ Variables for navigation bar dropdown menu """
-
-    restaurants = Restaurant.objects.all()
-    category_names = list({category.name for category in Category.objects.all()})
-    return {'all_restaurants': restaurants, 'all_category_names': category_names}
